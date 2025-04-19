64b1b6dcfe5381cd6aa6507380ecc99d9d1d40cb
darshangujraticodes <darshangujrati5@gmail.com>
2025-04-19 17:01:09 +0530

    dom manipulation

--- 03-DOM-manipulation/cart.js
+++ 03-DOM-manipulation/cart.js
@@ -9,14 +9,39 @@ const bagItemCounter = (items) => {
   cartCount.innerHTML = items.length;
 };
 
-const displayCartList = () => {
+const displayCartlist = (data) => {
   let cartProductList = "";
 
-  //   let cartHtmlItem = document.
+  data.forEach((item) => {
+    cartProductList += `
+      <div class="cartItemWrap mb-4">
+                <div class="cartItemCard row">
+                  <div class="col-2">
+                    <img src="${item.image}" class="imgfluid rounded-2" alt="${item.name}" />
+                  </div>
+                  <div class="col-8">
+                    <div class="productContentWrap">
+                      <h2 class="productName">${item.name}</h2>
+                      <p class="productInfo mb-1">
+                            ${item.info}
+                      </p>
+                      <p>
+                        <span class="discountPrice"> ₹ ${item.price.discountPrice} </span>
 
-  items.forEach((item) => {
-    cartProductList += ``;
+                        <span class="originalPrice">MRP <s>₹ ${item.price.originalPrice}  </s> </span>
+
+                        <span class="discountOff">(${item.discountOff}% OFF)</span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+    `;
   });
+
+  let cartHtmlItem = document.querySelector(".cartList");
+
+  cartHtmlItem.innerHTML = cartProductList;
 };
 
 const fetchDataList = (bagList) => {
@@ -43,7 +68,9 @@ function onLoad() {
 
   bagItemCounter(bagitems);
 
-  console.log(fetchDataList(bagitems));
+  const data = fetchDataList(bagitems);
+
+  displayCartlist(data);
 }
 
 onLoad();
